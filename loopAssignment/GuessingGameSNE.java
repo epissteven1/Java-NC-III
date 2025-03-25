ec7afa38572464b6a1733a7e0ff03e8c227bbef5
Steven <stevenepis9@gmail.com>
2025-03-25 21:15:20 +0800

    Fix Guessing Games

---
+++ loopAssignment/GuessingGameSNE.java
@@ -0,0 +1,31 @@
+package loopAssignment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGameSNE {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int numberToGuess = random.nextInt(50) + 1;
+        int numberOfAttempts = 0;
+        int guess = 0;
+
+        System.out.print("Guess a number between 1 and 50:");
+        while (guess != numberToGuess) {
+            guess = scanner.nextInt();
+            numberOfAttempts++;
+
+            if (guess < numberToGuess) {
+                System.out.print("Too low! Try again: ");
+            } else if (guess > numberToGuess) {
+                System.out.print("Too high! Try again: ");
+            } else {
+                System.out.print("Correct! You guessed it in " + numberOfAttempts + " attempts.");
+            }
+        }
+
+        scanner.close();
+    }
+}
